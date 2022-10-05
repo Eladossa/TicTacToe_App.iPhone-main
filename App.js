b9e67f58113f91c9cd9dd731cd8834773a125e22
Elad H <eladhtn@gmail.com>
2022-10-05 20:25:32 +0300

    2 URL changed

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Statistics from './screens/Statistics';
 import Game from './screens/Game';
 import ResultModal from './screens/ResultModal';
-const URL = 'http://www.whatyouwant.somee.com/api/Games/GamesList';
+const URL = 'http://www.wearetheworld.somee.com/api/Games/GamesList';
 
 // npm install  @react-navigation/native @react-navigation/native-stack
 
